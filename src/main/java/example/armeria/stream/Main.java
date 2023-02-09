7cefd1a0736dee08327d718ec08a087bfd7feadf
Youngseo Choi <ys550499@gmail.com>
2023-02-10 01:44:50 +0900

    file streaming response 연습

--- src/main/java/example/armeria/stream/Main.java
+++ src/main/java/example/armeria/stream/Main.java
@@ -1,18 +1,29 @@
 package example.armeria.stream;
 
-import com.linecorp.armeria.common.stream.ByteStreamMessage;
-import com.linecorp.armeria.common.stream.StreamWriter;
+import com.linecorp.armeria.common.HttpData;
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.HttpResponseWriter;
+import com.linecorp.armeria.common.ResponseHeaders;
 import com.linecorp.armeria.server.Server;
-import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.server.file.HttpFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws Exception {
+        Server server = newServer(8080);
+        server.closeOnJvmShutdown();
+        server.start().join();
+        logger.info("Server has been started. Serving dummy service at http://127.0.0.1:{}", server.activeLocalPort());
+    }
 
     static Server newServer(int port) throws Exception {
         URL resource = Main.class.getResource("/cantrbry/alice29.txt");
@@ -22,13 +33,34 @@ public class Main {
         return Server.builder()
                 .http(port)
                 .service("/bigfile", bigFile.asService())
+                .service("/bigfile2", (ctx, req) -> {
+                    HttpResponseWriter response = HttpResponse.streaming();
+                    response.write(ResponseHeaders.of(200));
+                    streamingFileResponse(response, new FileInputStream(new File(resource.toURI())));
+                    return response;
+                })
                 .build();
     }
 
-    public static void main(String[] args) throws Exception {
-        Server server = newServer(8080);
-        server.closeOnJvmShutdown();
-        server.start().join();
-        logger.info("Server has been started. Serving dummy service at http://127.0.0.1:{}", server.activeLocalPort());
+    private static void streamingFileResponse(HttpResponseWriter response, InputStream inputStream) {
+        byte[] chunk = new byte[8192];
+        try {
+            int length = inputStream.read(chunk);
+            if (length == -1) {
+                response.close();
+            } else {
+                response.whenConsumed().thenRun(() -> {
+                    if (response.tryWrite(HttpData.copyOf(chunk, 0, length))) {
+                        streamingFileResponse(response, inputStream);
+                    } else {
+                        // The response is completed unexpectedly.
+                        response.close();
+                    }
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            response.close();
+        }
     }
 }
